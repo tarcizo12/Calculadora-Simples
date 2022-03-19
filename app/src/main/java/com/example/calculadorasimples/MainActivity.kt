c7f1f2b2625b2e2870ce4d391286aa74513959d1
Tarcizo Neto <tarcizon12@gmail.com>
2022-03-19 18:35:06 -0300

    Inicializando Variáveis

--- app/src/main/java/com/example/calculadorasimples/MainActivity.kt
+++ app/src/main/java/com/example/calculadorasimples/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.calculadorasimples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addition: Button
+    private lateinit var multiplication: Button
+    private lateinit var division: Button
+    private lateinit var subtraction: Button
+    private lateinit var viewResult: TextView
+    private lateinit var numberOne: EditText
+    private lateinit var numberTwo: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.buttonOfSum)
+        multiplication = findViewById(R.id.buttonOfMultiplication)
+        division = findViewById(R.id.buttonOfDivision)
+        subtraction = findViewById(R.id.buttonOfSubtraction)
+
+        viewResult = findViewById(R.id.viewResult)
+        numberOne = findViewById(R.id.firstNumber)
+        numberTwo = findViewById(R.id.secondNumber)
+
 
     }
 }
